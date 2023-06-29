850c7b35745b5863b56b43a37692703a3cb6a341
Badraldientiriaki <47316456+Badraldientiriaki@users.noreply.github.com>
2023-06-30 00:55:03 +0300

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# ## In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# ## Some of the popular languages that Data Scientists use are: 
+# 1. Python 
+# 2. R 
+# 3. Java
+
+# | Data Science Tools |
+# |-------------------|
+# | Jupyter Notebook  |
+# | RStudio           |
+# | VS Code           |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python.
+
+# In[1]:
+
+
+(3*4)+5
+
+
+# In[2]:
+
+
+200/60
+
+
+# **Objectives:**
+# 
+# - List popular languages for Data Science
+# - Introduce common data science libraries
+# - Explore the Data Science Tools and Ecosystem
+# - Demonstrate evaluation of arithmetic expressions in Python
+# - Summarize key takeaways from the course
+
+# ## Author
+# Badr AlDien Tiriaki
